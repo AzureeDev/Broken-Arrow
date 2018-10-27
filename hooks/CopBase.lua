ebaf25803e18aa30450f393d55a1f6494084bf9f
Nepgearsy <timertimer5@gmail.com>
2018-10-27 06:38:43 +0200

    Fixed a shitton of stuff
    Don't put wald stuff anymore please

--- hooks/CopBase.lua
+++ hooks/CopBase.lua
@@ -1,7 +1,7 @@
 function CopBase:_chk_spawn_gear()
-
-    if self._tweak_table == "tank_hw" then --Disable glowy eyes on Headless Dozer
+    if self._tweak_table == "tank_hw" then --Disable glowy eyes on Headless Dozer -- Made a better lua, thanks shovi
     else
+        
         local align_obj_name = Idstring("Head")
         local align_obj = self._unit:get_object(align_obj_name)
         self._headwear_unit = World:spawn_unit(Idstring("units/zdann/ene_acc_zombie_eyes/ene_acc_zombie_eyes"), Vector3(), Rotation())
