65e050f11243ea8fc9d73ca3ba4fafbdc91bab1a
Nepgearsy <timertimer5@gmail.com>
2018-12-20 14:29:08 +0100

    tweaks and ee fixes

--- hooks/GroupAITweakData.lua
+++ hooks/GroupAITweakData.lua
@@ -3,16 +3,16 @@ Hooks:PostHook(GroupAITweakData, "_init_unit_categories", "zm_allow_clk_dozers",
         shield = 4,
         medic = 3,
         taser = 2,
-        tank = 8,
-        spooc = 16
+        tank = 6,
+        spooc = 4
     }
 end)
 
 Hooks:PostHook(GroupAITweakData, "_init_task_data", "zm_init_task_data", function(self)
     self.besiege.assault.force = {
-        48,
-        52,
-        56
+        56,
+        64,
+        70
     }
 
     self.besiege.assault.force_balance_mul = {
